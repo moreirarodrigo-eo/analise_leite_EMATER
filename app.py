9786bddd7c052e129cde84371266487f3ac5dfae
Rodrigo Moreira <108104791+moreirarodrigo-eo@users.noreply.github.com>
2025-05-28 13:57:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def load_data_media_geral():
     return gpd.read_file("data/STREAMLIT_media_leite_dia_Vaca_por_geom.geojson")
 gdf_geral = gpd.read_file("data/STREAMLIT_media_leite_dia_Vaca_por_geom.geojson")
 mean_by_year = gdf_geral.groupby(["Ano", "geometry"])["Informação_float"].mean().reset_index() 
-
+mean_by_year = gpd.GeoDataFrame(mean_by_year, geometry = "geometry", crs = "EPSG:4326")
 # Plot 1
 # Plot 1
 fig1 = px.scatter_mapbox(
