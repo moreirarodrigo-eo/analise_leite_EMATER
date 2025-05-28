56b466a821601792a60feaebcc94cc754a71c77b
Rodrigo Moreira <108104791+moreirarodrigo-eo@users.noreply.github.com>
2025-05-28 13:52:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,13 +34,13 @@ fig1 = px.scatter_mapbox(
 fig_violin = px.violin(
     mean_by_year,
     x="Ano",
-    y="Informação_float",
+    y="Produtividade (leite/dia/Vaca)",
     box=True,       # adds a mini boxplot inside the violin
     points="all",   # shows all individual points
     color="Ano",    # optionally color by year
     width=1200,
     height=800,
-    labels={"Informação_float": "(L/dia/vaca)", "Ano": "Ano"}
+    labels={"Produtividade (leite/dia/Vaca)": "(L/dia/vaca)", "Ano": "Ano"}
 )
 
 
@@ -50,8 +50,8 @@ st.plotly_chart(fig_violin, use_container_width=True)
 # Estatísticas do primeiro mapa
 st.subheader("Estatísticas Gerais da Produtividade")
 st.markdown(f"""
-- **Valor máximo:** {media_geral['Informação_float'].max():.2f} L/dia/vaca  
-- **Média:** {media_geral['Informação_float'].mean():.2f} L/dia/vaca
+- **Valor máximo:** {media_geral['Produtividade (leite/dia/Vaca)'].max():.2f} L/dia/vaca  
+- **Média:** {media_geral['Produtividade (leite/dia/Vaca)'].mean():.2f} L/dia/vaca
 """)
 
 
