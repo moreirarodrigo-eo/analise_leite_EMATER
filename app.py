33565acc65fe8a9e95b7c8c74742b6d44dd392a0
Rodrigo Moreira <108104791+moreirarodrigo-eo@users.noreply.github.com>
2025-05-28 08:36:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("Mapa de Produtividade de Leite por Vaca")
 # Load GeoDataFrame 1
 @st.cache_data
 def load_data_media_geral():
-    return gpd.read_file("STREAMLIT_media_leite_dia_Vaca_por_geom.geojson")
+    return gpd.read_file("data/STREAMLIT_media_leite_dia_Vaca_por_geom.geojson")
 
 media_geral = load_data_media_geral()
 
