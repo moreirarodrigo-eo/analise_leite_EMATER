5e50b45765c522c55a93b2858de308155577287a
Rodrigo Moreira <108104791+moreirarodrigo-eo@users.noreply.github.com>
2025-08-01 15:53:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -56,14 +56,15 @@ fig1 = px.scatter_mapbox(
 fig1.add_trace(go.Choroplethmapbox(
     geojson=pedology_json,
     locations=gdf_pedo.index,
-    z= [1]*len(gdf_pedo),  # dummy value to show color
+    z=[1]*len(gdf_pedo),  # Dummy value
+    colorscale=[[0, 'rgba(0,0,0,0)'], [1, 'rgba(0,0,0,0)']],  # Fully transparent fill
     showscale=False,
-    marker_opacity=0.3,
+    marker_opacity=0,  # Fully transparent
     marker_line_width=1,
+    marker_line_color='white',  # White outline
     hovertemplate="<b>Ordem</b>: %{customdata[0]}<br><b>Subordem</b>: %{customdata[1]}<extra></extra>",
     customdata=gdf_pedo[['ordem', 'subordem']],
-    name="Pedologia",
-    
+    name="Pedologia"
 ))
 
 fig1.update_layout(
