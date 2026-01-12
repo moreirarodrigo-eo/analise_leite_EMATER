c4b2095f004824346c917388ab822a565f96327a
Rodrigo Moreira <108104791+moreirarodrigo-eo@users.noreply.github.com>
2026-01-12 15:21:53 -0300

    Refactor marker size settings in fig2 traces

--- app.py
+++ app.py
@@ -171,7 +171,8 @@ fig2 = px.scatter_mapbox(
     animation_frame="Ano",
     title="Produtividade de Leite por Variedade de Capim ao Longo dos Anos"
 )
-fig2.update_traces(marker=dict(sizemin=1, sizemax=30))
+fig2.update_traces(marker=dict(sizemin=1))
+fig2.update_traces(marker=dict(sizemax=30))
 # # Add pedology layer (as fill)
 # # Create a separate trace for each ordem
 # for ordem, color in color_palette.items():
